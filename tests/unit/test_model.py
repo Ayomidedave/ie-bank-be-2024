647fcc0684a8cdff09ff1384237d11fc8befce47
Ayodave <ayomideadeakin@gmail.com>
2024-10-14 21:47:36 +0100

    Back end

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -13,3 +13,17 @@ def test_create_account():
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
+
+def test_create_account():
+    """
+    GIVEN a Account model
+    WHEN a new Account is created
+    THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
+    """
+    account = Account('David', 'country', 'currency')
+    assert account.name == 'John Doe'
+    assert account.country == 'Spain'
+    assert account.currency == '$'
+    assert account.account_number != None
+    assert account.balance == 0.0
+    assert account.status == 'Active'
